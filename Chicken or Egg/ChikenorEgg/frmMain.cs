70b48b98f4a1de0aafa1e5bb526cd9d8ab04b9c6
Stephen Omotayo <127545231+StephTayo@users.noreply.github.com>
2023-05-11 14:30:05 -0600

    Add files via upload

---
+++ Chicken or Egg/ChikenorEgg/frmMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChickenOrEgg
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // user selected chicken
+        private void rdoChicken_CheckedChanged(object sender, EventArgs e)
+        {
+            picAnswer.Image = Image.FromFile(@"..\..\Images\chicken.jpg");
+        }
+
+        // user selected egg
+        private void rdoEgg_CheckedChanged(object sender, EventArgs e)
+        {
+            picAnswer.Image = Image.FromFile("../../Images/egg.jpg");
+        }
+
+        // start with no selection
+        
+
+        // no answer
+        private void rdoNeither_CheckedChanged(object sender, EventArgs e)
+        {
+            picAnswer.Image = null; // no value
+        }
+
+        // user selected from combo box
+        // items in combo box are indexed 0, 1, ...
+        private void cboAnswer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cboAnswer.SelectedIndex)
+            {
+                case 0: // Chicken
+                    picAnswer.Image = Image.FromFile(@"..\..\Images\chicken.jpg");
+                    break;
+                case 1: // Egg
+                    picAnswer.Image = Image.FromFile(@"..\..\Images\egg.jpg");
+                    break;
+                default: 
+                    picAnswer.Image = null;
+                    break;
+            }
+        }
+    }
+}
